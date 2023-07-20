844596dd2defdadf8cbc5d0504f2a889195a1813
SummerDev <ivosummer@gmail.com>
2023-07-20 09:20:30 +0200

    repo update

--- main.js
+++ main.js
@@ -1,174 +1,180 @@
-var beauticianMenu, beauticianContent
-var initialService, initialServiceItemId
+var menuContainer = document.createElement('div')
+var serviceInnerContainer = document.createElement('div')
+menuContainer.classList = 'services__container-menu'
+serviceInnerContainer.classList ='services__container-content'
+
+var serviceContainer
+var initialServiceId = 1
+var actialService
 
 const init = () => {
-    beauticianMenu = document.getElementById('serviceMenu')
-    beauticianContent = document.getElementById('serviceContent')
-
-    initialService = services[0]
-    initialServiceItemId = 1
-
-    let menu = createServiceMenu(initialService)
-    updateMenu(menu)
-    let content = createSelectedServiceList(initialServiceItemId)
-    updateContent(content)
+    serviceContainer = document.getElementById('serviceContainer')
+    serviceContainer.append(...[menuContainer, serviceInnerContainer])
+    getService(initialServiceId)
+    createServiceInner(initialServiceId)
 }
 
-const updateMenu = (content) => {
-    beauticianMenu.append(content)
+const updateMenu = (menu) => {
+    menuContainer.innerHTML = ''
+    menuContainer.append(...menu)
 }
 
 const updateContent = (content) => {
-    beauticianContent.append(content)
+    serviceInnerContainer.innerHTML = ''
+    serviceInnerContainer.append(content)
 }
  
-const getService = (element) => {
-    let serviceIndex = element ? +element?.dataset.index : 0
-    initialService = services[serviceIndex]
-
-    let content = createServiceMenu(initialService)
+const getService = (id) => {
+    actialService = services.find(serv => serv.id === id)
+    let content = createServiceOption(actialService)
     updateMenu(content)
 }
 
-const createServiceMenu = (service) => {
-    beauticianMenu.innerHTML = ''
-
-    let menuList = document.createElement('ul')
-
-    menuList.classList = 'services__container-menu-list'
-
-    for (let index = 0; index < service.length; index++) {
-        let item = service[index]
-        let menuItem = document.createElement('li')
-        let menuItemBtn = document.createElement('button')
-        menuItem.classList = 'services__container-menu-list-item'
-        menuItemBtn.classList = 'services__container-menu-list-item-btn'
-        menuItemBtn.append(item?.title)
-        menuItemBtn.value = item.id
-        menuItemBtn.onclick = function () {
-            createSelectedServiceList(+this.value)
-        }
-        menuItem.append(menuItemBtn)
-        menuList.append(menuItem)
+const createServiceOption = (service) => {
+    const size = service.serv.length
+    let menuLabel = document.createElement('label')
+    let menuSelect = document.createElement('select')
+    
+    menuLabel.textContent = service.serviceType
+    menuLabel.classList = 'services__container-menu-label'
+    menuSelect.classList = 'services__container-menu-select'
+    menuSelect.onchange = function() {
+        createServiceInner(+this.value)
     }
 
-    return menuList
+    for (let index = 0; index < size; index++) {
+        let serviceitem = service?.serv[index]
+        let menuItem = document.createElement('option')
+        menuItem.classList = 'services__container-menu-select-option'
+        menuItem.value = serviceitem.id
+        menuItem.text = serviceitem.title
+        menuSelect.append(menuItem)
+    }
+
+    return [menuLabel, menuSelect]
 }
 
-const createSelectedServiceList = (id) => {
-    beauticianContent.innerHTML = ''
-    let serviceInner = initialService.find(service => service.id === id).services
-    let serviceContent = document.createElement('div')
-    serviceContent.classList = 'services__container-content'
-    serviceContent.id = 'serviceContent'
+const createServiceInner = (id) => {
+    let innerService = actialService.serv.find(s => s.id === id)
+    console.log(innerService)
+    let size = innerService.services.length
     let serviceList = document.createElement('ul')
+
     serviceList.classList = 'services__container-content-list'
 
-    for (let index = 0; index < serviceInner.length; index++) {
-        const item = serviceInner[index]
+    for (let index = 0; index < size; index++) {
+        const item = innerService.services[index]
         let serviceListItem = document.createElement('li')
-        serviceListItem.classList = 'services__container-content-list-item'
-
         let titleSpan = document.createElement('span')
-        titleSpan.classList = 'title'
         let priceSpan = document.createElement('span')
-        priceSpan.classList = 'price'
+
+        serviceListItem.classList = 'row services__container-content-list-item'
+        titleSpan.classList = 'col-12 col-sm-10 title'
+        priceSpan.classList = 'col-12 col-sm-2 price'
 
         titleSpan.append(item?.title)
-        priceSpan.append(item?.price)
+        priceSpan.append(`${item?.price} Ft.-`)
         serviceListItem.append(titleSpan, priceSpan)
         serviceList.append(serviceListItem)
     }
 
-    return serviceList
+    updateContent(serviceList)
 }
 
-let beautician = [
-    {
-        id: 1,
-        title: 'Arckezelések',
-        discription: null,
-        services: [
-            {
-                id: 1,
-                title: 'Tini kezelés (18 éves korig)',
-                price: 6500,
-                discription:'Letisztítás, peeling, gőzőlés, nyomkodás/pórusok kitisztítása, fertőtlenítés, összehúzó, nyugtató pakolás, befejező krém.',
-                extras: []
-            },
-            {
-                id: 2,
-                title: 'Nagykezelés',
-                price: 9000,
-                discription:'Letisztítás, peeling, gőzőlés, nyomkodás/pórusok kitisztítása, fertőtlenítés, összehúzó, nyugtató pakolás, befejező krém.',
-                extras: [
-                    {
-                        id: 1,
-                        title: 'UH/lontoforézis hatóanyag bevitel',
-                        price: 2000
-                    },
-                    {
-                        id: 2,
-                        title: '15 perc arc, nyak és dekoltázsmasszázs',
-                        price: 2000
-                    },
-                    {
-                        id: 3,
-                        title: 'Aranymaszk',
-                        price: 2000
-                    },
-                    {
-                        id: 4,
-                        title: 'Peptides szemkörnyék feltöltő',
-                        price: 4500
-                    }
-                ]
-            },
-            {
-                id: 3,
-                title: 'Intenzív tápláló mélyhidratáló kezelés',
-                price: 7900,
-                discription: null,
-                extras: []
-            },
-            {
-                id: 4,
-                title: 'Arc, nyak és dekoltázsmasszázs',
-                price: 4900,
-                discription: null,
-                extras: []
-            },
-            {
-                id: 5,
-                title: 'Kiskezelés',
-                price: 7500,
-                discription: 'Letisztítás, peeling, masszázs, tápláló pakolás, befejező krém.',
-                extras: []
-            },
-            {
-                id: 6,
-                title: 'Hidroabrázió',
-                price: 2500,
-                discription: null,
-                extras: []
-            },
-            {
-                id: 7,
-                title: 'Microdermabrázió',
-                price: 4500,
-                discription: 'Gyémántfejes bőr csiszolás.',
-                extras: []
-            }
-        ]
-    },
-    {
-        id: 2,
-        title: 'Prémium anti-aging kezelések',
-        discription: null,
-        services: {
 
+let beautician = {
+    id: 1,
+    serviceType: 'Kozmetika',
+    name: 'Rita',
+    phone: '+36704567890',
+    serv: [
+        {
+            id: 1,
+            title: 'Arckezelések',
+            discription: null,
+            services: [
+                {
+                    id: 1,
+                    title: 'Tini kezelés (18 éves korig)',
+                    price: 6500,
+                    discription:'Letisztítás, peeling, gőzőlés, nyomkodás/pórusok kitisztítása, fertőtlenítés, összehúzó, nyugtató pakolás, befejező krém.',
+                    extras: []
+                },
+                {
+                    id: 2,
+                    title: 'Nagykezelés',
+                    price: 9000,
+                    discription:'Letisztítás, peeling, gőzőlés, nyomkodás/pórusok kitisztítása, fertőtlenítés, összehúzó, nyugtató pakolás, befejező krém.',
+                    extras: [
+                        {
+                            id: 1,
+                            title: 'UH/lontoforézis hatóanyag bevitel',
+                            price: 2000
+                        },
+                        {
+                            id: 2,
+                            title: '15 perc arc, nyak és dekoltázsmasszázs',
+                            price: 2000
+                        },
+                        {
+                            id: 3,
+                            title: 'Aranymaszk',
+                            price: 2000
+                        },
+                        {
+                            id: 4,
+                            title: 'Peptides szemkörnyék feltöltő',
+                            price: 4500
+                        }
+                    ]
+                },
+                {
+                    id: 3,
+                    title: 'Intenzív tápláló mélyhidratáló kezelés',
+                    price: 7900,
+                    discription: null,
+                    extras: []
+                },
+                {
+                    id: 4,
+                    title: 'Arc, nyak és dekoltázsmasszázs',
+                    price: 4900,
+                    discription: null,
+                    extras: []
+                },
+                {
+                    id: 5,
+                    title: 'Kiskezelés',
+                    price: 7500,
+                    discription: 'Letisztítás, peeling, masszázs, tápláló pakolás, befejező krém.',
+                    extras: []
+                },
+                {
+                    id: 6,
+                    title: 'Hidroabrázió',
+                    price: 2500,
+                    discription: null,
+                    extras: []
+                },
+                {
+                    id: 7,
+                    title: 'Microdermabrázió',
+                    price: 4500,
+                    discription: 'Gyémántfejes bőr csiszolás.',
+                    extras: []
+                }
+            ]
+        },
+        {
+            id: 2,
+            title: 'Prémium anti-aging kezelések',
+            discription: null,
+            services: {
+    
+            }
         }
-    }
-]
+    ]
+} 
+
 
 let services = [beautician]
